a53f5564d3f61682ca15b8b22fb23860b38587ed
TonNomGitHub <tonEmailAssocie@exemple.com>
2026-02-19 16:08:19 +0000

    correction erreur de style login

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
+  constructor(private router: Router) {}
 
+  login() {
+    this.router.navigate(['/admin/dashboard']);
+  }
 }
